2ffbdbfe11c95d03e31c97d20daa6f88ad8f9761
htthangcse <hatienthangk22cse@gmail.com>
2025-06-09 17:00:13 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,10 @@ async function bootstrap() {
     credentials: true, // Nếu bạn cần gửi cookie / header
   });
   app.useGlobalPipes(new ValidationPipe());
-  // await app.listen(env.APP_PORT ?? 3000);
-  await app.listen(3000);
+  await app.listen(env.APP_PORT ?? 3000, () => {
+    console.log('Server is running on port', env.APP_PORT ?? 3000);
+  });
+  // await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
